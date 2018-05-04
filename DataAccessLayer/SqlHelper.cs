380e1f5b64bef7663b38c235eb6068b6ecb12cf1
OnurSevket <onursevket44@gmail.com>
2018-05-04 23:16:59 +0300

    First Commit
    I made four Layered Architecture
    *has been created Model
    *has been created sqlHelper

---
+++ DataAccessLayer/SqlHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static class SqlHelper
+    {
+        public static Func<DbConnection> ConnectionFactory = () => new SqlConnection(ConnectionString.Connection);
+
+        public static class ConnectionString
+        {
+            public static string Connection = ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString;
+        }
+
+        public static class AppSettings
+        {
+            public static string appSettingVal = System.Configuration.ConfigurationManager.AppSettings["DebugOutlineActive"];
+        }
+
+    }
+}
