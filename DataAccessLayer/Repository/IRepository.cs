7143c65b7f455756f1acb502963f540815860a52
Onur Özdemir <onursevket44@gmail.com>
2018-05-12 00:16:01 +0300

    The Interface Repository has been created for Data Access Layer

---
+++ DataAccessLayer/Repository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    interface IRepository<TEntity> where TEntity : class
+    {
+
+        TEntity Get(int Id);
+        TEntity Get(Expression<Func<TEntity, bool>> where);
+        ICollection<TEntity> GetAll();
+        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> where);
+        int InsertOrUpdate(TEntity entity, out int? insertedId);
+        bool Delete(TEntity entity);
+        bool Delete(Expression<Func<TEntity, bool>> where);
+
+
+    }
+}
