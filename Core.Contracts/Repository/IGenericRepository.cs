8d3f264ce707b782a3937421a214eb3d6a2e7cc2
Onur Özdemir <onursevket44@gmail.com>
2018-05-17 16:01:21 +0300

    has been created Core Layered

---
+++ Core.Contracts/Repository/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Contracts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Contracts.Repository
+{
+    public interface IGenericRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> predicate = null);
+        T Get(Expression<Func<T, bool>> predicate);
+        T Add(T entity);
+        T Update(T entity);
+        int Delete(T entity);
+    }
+}
---
+++ Core/Repository/Dapper/DapperGenericRepository.cs
@@ -0,0 +1,122 @@
+﻿using Core.Contracts.Entities;
+using Core.Contracts.Repository;
+using Core.Enum;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repository.Dapper
+{
+    public abstract class DapperGenericRepository<TEntity> : IGenericRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        public abstract IDbConnection Connection { get; }
+
+        public abstract string TableName { get; }
+
+        public TEntity Add(TEntity entity)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                SqlMapperExtensions.TableNameMapper = (type) =>
+                {
+                    switch (type.Name)
+                    {
+                        default:
+                            return TableName;
+                    }
+                };
+
+                conn.Open();
+                conn.Insert(entity);
+                conn.Close();
+                return entity;
+            }
+        }
+
+        public int Delete(TEntity entity)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                SqlMapperExtensions.TableNameMapper = (type) =>
+                {
+                    switch (type.Name)
+                    {
+                        default:
+                            return TableName;
+                    }
+                };
+
+                conn.Open();
+                var result = conn.Delete(entity);
+                conn.Close();
+                return result == true ? (int)AdoptedEnums.Achievement.success : (int)AdoptedEnums.Achievement.fail;
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            TEntity entity = null;
+            DynamicQueryResult result = GenerateDynamicQuery.GetDynamicQuery(TableName, predicate);
+            using (IDbConnection conn = Connection)
+            {
+                conn.Open();
+                entity = conn.Query<TEntity>(result.Sql, (object)result.Parameter).FirstOrDefault();
+                conn.Close();
+                return entity;
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            List<TEntity> entityList = null;
+            if (predicate == null)
+            {
+                using (IDbConnection conn = Connection)
+                {
+                    conn.Open();
+                    entityList = conn.Query<TEntity>("SELECT * FROM " + TableName).ToList();
+                    conn.Close();
+                    return entityList;
+                }
+            }
+            else
+            {
+                DynamicQueryResult result = GenerateDynamicQuery.GetDynamicQuery(TableName, predicate);
+                using (IDbConnection conn = Connection)
+                {
+                    conn.Open();
+                    entityList = conn.Query<TEntity>(result.Sql, (object)result.Parameter).ToList();
+                    conn.Close();
+                    return entityList;
+                }
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                SqlMapperExtensions.TableNameMapper = (type) =>
+                {
+                    switch (type.Name)
+                    {
+                        default:
+                            return TableName;
+                    }
+                };
+
+                conn.Open();
+                conn.Update(entity);
+                conn.Close();
+                return entity;
+            }
+        }
+    }
+}
