1ced498598a198c8c1b940855680e0a2d12f4a06
Onur Özdemir <onursevket44@gmail.com>
2018-05-18 09:39:36 +0300

    DataAccessLayer.Contracts has been created

---
+++ DataAccessLayer.Contracts/IParkAreaRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Contracts.Repository;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Contracts
+{
+    public interface IParkAreaRepository:IGenericRepository<ParkArea>
+    {
+
+
+    }
+}
